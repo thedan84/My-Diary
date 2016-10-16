df8ecef0a6f2a1d5b371760fbb38024906d59e1b
Dennis Parussini <thedan84@bitbucket.org>
2016-10-16 19:54:50 +0200

    Initial project setup.

---
+++ My Diary/EntryCell.swift
@@ -0,0 +1,24 @@
+//
+//  EntryCell.swift
+//  My Diary
+//
+//  Created by Dennis Parussini on 16-10-16.
+//  Copyright © 2016 Dennis Parussini. All rights reserved.
+//
+
+import UIKit
+
+class EntryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
